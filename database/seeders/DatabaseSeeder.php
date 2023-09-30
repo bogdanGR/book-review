983aa6bb8e3215f2048b1e5723fcec2cc80e12b9
Bogdan450 <b.vaskan@therightclick.gr>
2023-09-30 14:11:57 +0300

    init project, create Book/Review models, migrations and factories

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Review;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Book::factory(33)->create()->each(function ($book) {
+           $numReview = random_int(5, 30);
+
+           Review::factory()->count($numReview)
+               ->good()
+               ->for($book)
+               ->create();
+        });
+    }
+}
