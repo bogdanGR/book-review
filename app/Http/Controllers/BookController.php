2f55bca17c342dfa3ba09208cb57b5af87e6fcde
Bogdan450 <b.vaskan@therightclick.gr>
2023-10-02 22:16:23 +0300

    implement the filters

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,8 +13,19 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
-        $books = Book::when($title, fn ($query, $title) => $query->title($title) )->get();
+        $filter = $request->input('filter', '');
 
+        $books = Book::when($title, fn ($query, $title) => $query->title($title));
+
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_six_months' => $books->popularLastSixMonths(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_six_months' => $books->highestRatedLastSixMonths(),
+            default => $books->latest()
+        };
+
+        $books = $books->get();
 
         return view('books.index', ['books' => $books]);
     }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -28,7 +28,7 @@ class Book extends Model
         return $query->withAvg(['reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)], 'rating')
             ->orderBy('reviews_avg_rating', 'desc');
     }
-    public function scopeMinReviews(Builder $query, int $minReviews):Builder|QueryBuilder
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
     {
         return $query->having('reviews_count', '>=', $minReviews);
     }
@@ -41,6 +41,29 @@ class Book extends Model
         } elseif (isset($from) && isset($to)) {
             $query->whereBetween('created_at', [$from, $to]);
         }
-
+    }
+    public function scopePopularLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+    public function scopePopularLastSixMonths(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+    public function scopeHighestRatedLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+    public function scopeHighestRatedLastMonthSixMonths(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now())
+            ->minReviews(5);
     }
 }
